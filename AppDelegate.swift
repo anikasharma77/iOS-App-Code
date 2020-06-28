3fc35a417a241c22c4f4af0f7464bb3ddf1f3a41
anikasharma77 <67562538+anikasharma77@users.noreply.github.com>
2020-06-28 16:10:31 -0700

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  NotificationsUI
+//
+//  Created by Pranjal Satija on 9/12/16.
+//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  CSPillTracker
+//
+//  Created by Anika Sharma on 1/5/19.
+//  Copyright © 2019 Anika Sharma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var ButtonNextPage: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
